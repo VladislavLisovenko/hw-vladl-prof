05522f3dc3ea99734d7ef54138286f8cbafeaf00
VladislavLisovenko <vla78@bk.ru>
2024-02-18 12:56:37 +0700

    hw03_frequency_analysis is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,67 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"slices"
+	"strings"
+)
+
+type WordFrequency struct {
+	w  string
+	fr int
+}
+
+func wordsToSlice(m map[string]int) []WordFrequency {
+	res := make([]WordFrequency, 0)
+	for k, v := range m {
+		res = append(res, WordFrequency{
+			w:  k,
+			fr: v,
+		})
+	}
+
+	return res
+}
+
+func Top10(s string) []string {
+	if s == "" {
+		return []string{}
+	}
+
+	s = strings.ReplaceAll(s, "\n", " ")
+	s = strings.ReplaceAll(s, "\t", " ")
+
+	words := strings.Split(s, " ")
+
+	m := make(map[string]int)
+	for _, word := range words {
+		if word == "" {
+			continue
+		}
+		m[word] = m[word] + 1
+	}
+	sl := wordsToSlice(m)
+
+	slices.SortFunc[[]WordFrequency](sl, func(a, b WordFrequency) int {
+		if a.fr == b.fr {
+			return strings.Compare(a.w, b.w)
+		}
+		switch {
+		case a.fr > b.fr:
+			return -1
+		case a.fr < b.fr:
+			return 1
+		default:
+			return 0
+		}
+	})
+
+	res := []string{}
+	for i, v := range sl {
+		if i == 10 {
+			break
+		}
+		res = append(res, v.w)
+	}
+
+	return res
 }
