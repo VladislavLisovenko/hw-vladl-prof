ce72bbfcc43a93d95e6729e898dedd462ac4ba6f
VladislavLisovenko <vla78@bk.ru>
2024-03-11 14:44:56 +0700

    hw05_parallel_execution is completed

--- hw05_parallel_execution/go.mod
+++ hw05_parallel_execution/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw05_parallel_execution
+module github.com/VladislavLisovenko/hw-vladl-prof/hw05_parallel_execution
 
 go 1.19
 
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,28 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var errCount int32
+	var wg = sync.WaitGroup{}
+	limit := make(chan struct{}, n)
+
+	for _, t := range tasks {
+		task := t
+		if m > 0 && int(errCount) >= m {
+			return ErrErrorsLimitExceeded
+		}
+		limit <- struct{}{}
+		wg.Add(1)
+		go func() {
+			err := task()
+			if m > 0 && err != nil {
+				atomic.AddInt32(&errCount, 1)
+			}
+			<-limit
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
 	return nil
 }
