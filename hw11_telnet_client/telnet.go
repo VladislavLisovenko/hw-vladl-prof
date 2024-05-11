9141c8985db6745f557d22ce7f56ae1aff1bfd1f
VladislavLisovenko <vla78@bk.ru>
2024-05-11 09:50:37 +0700

    hw11_telnet_client is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +14,56 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type client struct {
+	address    string
+	timeout    time.Duration
+	in         io.ReadCloser
+	out        io.Writer
+	connection net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (c *client) Connect() error {
+	connection, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return fmt.Errorf("connection error: %w", err)
+	}
+	fmt.Printf("connected to %s\n", c.address)
+	c.connection = connection
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *client) Send() error {
+	if _, err := io.Copy(c.connection, c.in); err != nil {
+		return fmt.Errorf("send error: %w", err)
+	}
+	fmt.Println("data sent")
+	return nil
+}
+
+func (c *client) Receive() error {
+	if _, err := io.Copy(c.out, c.connection); err != nil {
+		return fmt.Errorf("receive error: %w", err)
+	}
+	fmt.Println("data were received")
+	return nil
+}
+
+func (c *client) Close() error {
+	if c.connection != nil {
+		if err := c.connection.Close(); err != nil {
+			return fmt.Errorf("connection closing error: %w", err)
+		}
+	}
+	fmt.Println("connection closed")
+	return nil
+}
